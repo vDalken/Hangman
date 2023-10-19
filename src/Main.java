a274843596429b5ea8f0c518906dba6deeaf6649
rodgreg02 <mindera@ME00391.local>
2023-10-19 17:00:16 +0100

    added array hiddenWord and wordArray, added logic to hide the word with a different array printing _. Deleted int in method getRandomWord and simplified it to random

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,29 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
         String word = getRandomWord();
-        System.out.println(word);
-        for(int index=0;  index<word.length();index++){
-
+        char[] charArray = new char[word.length()];
+        for (int i = 0; i < word.length(); i++) {
+            charArray[i] = word.charAt(i);
         }
+        Render(charArray);
+
     }
+    //Used to pick a random word from the pre defined array of words
     public static String getRandomWord(){
         String[] words = {"comunicar","roto","maravilha","milagroso","olhar","cadeira","lugares","escada","janela","cabelo"};
         Random random = new Random();
-        int randomNumber = random.nextInt(0,11);
-        return words[randomNumber];
+        return words[random.nextInt(0,9)];
+    }
+
+   public static char[] Render(char[] wordArray){
+        char[] hiddenWord = new char[wordArray.length];
+       for (int i = 0; i < wordArray.length; i++) {
+           hiddenWord[i] = '_';
+       }
+       for (int i = 0; i < hiddenWord.length; i++) {
+           System.out.println(hiddenWord[i]);
+           System.out.println(wordArray[i]);
+       }
+return null;
     }
 }
